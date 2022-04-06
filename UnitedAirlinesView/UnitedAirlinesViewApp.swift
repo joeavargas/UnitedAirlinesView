8df96a5e3b690afc86f216bb48b5a54e648fe91e
Joe Vargas <joeavargas@outlook.com>
2022-04-06 11:03:12 -0500

    Import skeleton project

---
+++ UnitedAirlinesView/UnitedAirlinesViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitedAirlinesViewApp.swift
+//  UnitedAirlinesView
+//
+//  Created by Joe Vargas on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitedAirlinesViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
