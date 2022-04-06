fabd48374c3dcee42504976b7e6edbd898e39db3
Joe Vargas <joeavargas@outlook.com>
2022-04-06 13:02:34 -0500

    Header added and configured

--- UnitedAirlinesView/Screens/MyUnitedView.swift
+++ UnitedAirlinesView/Screens/MyUnitedView.swift
@@ -9,7 +9,36 @@ import SwiftUI
 
 struct MyUnitedView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // MARK: - Header with profile image, salutation and profile/settings link
+        HStack {
+            Image(systemName: "person")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 50, height: 50)
+                .background(Color.white)
+                .foregroundColor(.blue)
+                .clipShape(Circle())
+                .padding()
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Hello, John")
+                    .font(.system(size: 22))
+                    .fontWeight(.medium)
+                Text("View profile and settings >")
+                    .foregroundColor(.blue)
+            }
+            Spacer()
+        }
+        .frame(height: 130)
+        .background(Color.white)
+        
+        // MARK: - Card view section
+        
+        // MARK: - Upcoming trip section
+        
+        // MARK: - Do you have a known traveler number section
+        
+        // MARK: - Find everything ok?
     }
 }
 
